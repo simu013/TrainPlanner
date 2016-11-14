7a8b10d3222a6c6738269327e2861b5986a6ba80
Simon <Simon@172.16.3.148>
2016-11-14 01:02:42 +0100

    Socket Prototyp added

--- TrainPlanner/src/ch/abbts/szskfh/trainplanner/client/Client.java
+++ TrainPlanner/src/ch/abbts/szskfh/trainplanner/client/Client.java
@@ -5,6 +5,13 @@
  */
 package ch.abbts.szskfh.trainplanner.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  *
  * @author Simon
@@ -15,7 +22,31 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String ipAdress = "Localhost";
+        short portNr = 5432;
+        String receiveString = null;
+        
+        try {
+            Socket socket = new Socket(ipAdress, portNr);
+
+            // Streams verbinden
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            out.println("Hallo ich bin der Client"); // senden
+            receiveString = in.readLine(); // empfangen
+            System.out.println("empfangen: " + receiveString); // Daten vom Server zu Testzwecken auf Console ausgeben. 
+            out.println("exit");
+            out.flush();            
+            
+            // Streams und Socket schliessen
+            in.close();
+            out.close();
+            socket.close();
+            
+        } catch (IOException ex) {
+            System.out.println("Exception: " + ex.toString());
+        }
     }
     
 }
--- TrainPlanner/src/ch/abbts/szskfh/trainplanner/server/Server.java
+++ TrainPlanner/src/ch/abbts/szskfh/trainplanner/server/Server.java
@@ -5,16 +5,69 @@
  */
 package ch.abbts.szskfh.trainplanner.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 /**
  *
  * @author Simon
  */
 public class Server {
-    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ServerSocket serverSocket = null;
+        short portNr = 5432;
+        String receiveString = null;
+        String connect = null;
+        
+        // Socket bereitstellen
+        try {
+            serverSocket = new ServerSocket(portNr); 
+        } 
+        catch (IOException ex) {
+            System.out.println("Exception: " + ex.toString());
+        }
+        
+        // Auf Client Verbindung warten
+        try {
+            System.out.println("READY");
+            Socket socket = serverSocket.accept();
+            
+            if (socket.isBound()) {
+                System.out.println("OK");
+            }
+                
+            // IO Streams verbinden
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+            
+            // Datenaustausch
+            do {
+                out.println("Hallo ich bin der Server"); // senden
+                out.flush();
+                receiveString = in.readLine(); // Daten von CLient empfangen
+                
+                System.out.println("empfangen: " + receiveString); // Daten von Client zum testen auf Console ausgeben. 
+                
+            } 
+            while(receiveString.equals("exit") == false); // Verbindung halten bis EXIT geliefert wird. 
+            
+            // Socket und Streams schliessen. 
+            in.close();
+            out.close();
+            socket.close();
+            
+        } 
+        catch (IOException ex) { // Ausnahmen abfangen 
+            System.out.println("Exception: " + ex.toString());
+        }
     }
 }
